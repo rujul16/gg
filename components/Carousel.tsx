61caf6ab6f0687c99f3cc654f4c2032733c4126b
rujul16 <pritwanirujul16072005@gmail.com>
2024-08-16 12:47:57 +0530

    ui fixes

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,68 +1,74 @@
-"use client"
-import React,{useEffect, useState} from 'react'
-import { Button } from './ui/button';
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import "./Carousel.css"; // Import your CSS file
 
-
-const Carousel = ({slides}:carouselProps) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    const slideStyle = {
-        width:'100%',
-        height:'100%',
-        backgroundImage:`url(${slides[currentIndex].url})`,
-        borderRadius:'10px',
-        backgroundSize:'cover'
-
-    };
-    const sliderStyle = {
-        
-        height:'100%',
-        position:'relative'
-
-    };
-    const ArrowStyles = {
-        
-        top:'50%',
-        position:'absolute',
-        transform:'translate(0,-50%)',
-        height:'10%'
-        
-
-    };
-    const goToPrevious = ()=>{
-        const isFirst = currentIndex===0;
-        const newIndex = isFirst? slides.length-1:currentIndex-1;
-        setCurrentIndex(newIndex);
-    };
-    const goToNext = ()=>{
-        const isLast = currentIndex===slides.length-1;
-        const newIndex = isLast? 0:currentIndex+1;
-        setCurrentIndex(newIndex);
-    };
-    const goToSlide = (slideIndex)=>{
-        setCurrentIndex(slideIndex);
-    };
-
-    useEffect(()=>{
-        const timeInterval = setInterval(()=>{goToNext();console.log('next');},2000);
-        return()=>{
-            clearInterval(timeInterval);
-        }
-    })
-    
-
-
-  return (
-    <div style={sliderStyle}>
-        <Button style={ArrowStyles} className='left-4 bg-transparent font-bold' onClick={goToPrevious}>{'<'}</Button>
-        <Button style={ArrowStyles} className='right-4 bg-transparent' onClick={goToNext}>{'>'}</Button>
-        <div style={slideStyle}></div>
-        <div className='flex justify-center gap-2'>{slides.map((slide,slideIndex)=>(
-            <div key={slideIndex} className='hover:cursor-pointer' onClick={() => goToSlide(slideIndex)}>⬤</div>
-        )
-        )}</div>
-    </div>
-  )
+// Define the types for the slides and props
+interface Slide {
+  url: string;
 }
 
-export default Carousel
+interface CarouselProps {
+  slides: Slide[];
+}
+
+const Carousel = ({ slides }: CarouselProps) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  // Inline style for background image of the current slide
+  const slideStyle = {
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
+
+  const goToPrevious = () => {
+    const isFirst = currentIndex === 0;
+    const newIndex = isFirst ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLast = currentIndex === slides.length - 1;
+    const newIndex = isLast ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      goToNext();
+      console.log("next");
+    }, 2000);
+
+    return () => {
+      clearInterval(timeInterval);
+    };
+  }, [currentIndex]);
+
+  return (
+    <div className="slider-container">
+      <Button className={`arrow arrow-left`} onClick={goToPrevious}>
+        {"<"}
+      </Button>
+      <Button className={`arrow arrow-right`} onClick={goToNext}>
+        {">"}
+      </Button>
+      <div className="slide" style={slideStyle}></div>
+      <div className="flex justify-center gap-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            className="hover:cursor-pointer"
+            onClick={() => goToSlide(slideIndex)}
+          >
+            ⬤
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,60 +1,115 @@
-'use client'
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { usePathname, useRouter } from 'next/navigation';
+"use client";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 const Navbar = () => {
   const router = useRouter();
   const currentRoute = usePathname();
 
   return (
-    <nav className="flex justify-between items-center p-4">
+    <nav className="flex justify-between items-center p-4 ">
       {/* Logo */}
       <Link href="/">
-        <Image src="/icons/logo.png" alt="logo" height={80} width={150} className="cursor-pointer" />
+        <Image
+          src="/icons/logo.png"
+          alt="logo"
+          height={80}
+          width={150}
+          className="cursor-pointer"
+        />
       </Link>
 
       {/* Navigation Links */}
       <ul className="flex gap-6 items-center">
-        <li>
-          <Link href="/" className={currentRoute === '/' ? 'text-[#d69ef7] font-bold' : 'hover:text-[#d69ef7]'}>
+        <li className="hover:scale-105">
+          <Link
+            href="/"
+            className={
+              currentRoute === "/"
+                ? "text-[#d69ef7] font-bold"
+                : "hover:text-[#d69ef7]"
+            }
+          >
             Home
           </Link>
         </li>
-        <li>
-          <Link href="/companies" className={currentRoute === '/companies' ? 'text-[#d69ef7] font-bold' : 'hover:text-[#d69ef7]'}>
+        <li className="hover:scale-105">
+          <Link
+            href="/companies"
+            className={
+              currentRoute === "/companies"
+                ? "text-[#d69ef7] font-bold"
+                : "hover:text-[#d69ef7]"
+            }
+          >
             Companies
           </Link>
         </li>
-        <li>
-          <Link href="/events" className={currentRoute === '/events' ? 'text-[#d69ef7] font-bold' : 'hover:text-[#d69ef7]'}>
+        <li className="hover:scale-105">
+          <Link
+            href="/events"
+            className={
+              currentRoute === "/events"
+                ? "text-[#d69ef7] font-bold"
+                : "hover:text-[#d69ef7]"
+            }
+          >
             Events
           </Link>
         </li>
-        <li>
-          <Link href="/authority-connect" className={currentRoute === '/authority-connect' ? 'text-[#d69ef7] font-bold' : 'hover:text-[#d69ef7]'}>
+        <li className="hover:scale-105">
+          <Link
+            href="/authority-connect"
+            className={
+              currentRoute === "/authority-connect"
+                ? "text-[#d69ef7] font-bold"
+                : "hover:text-[#d69ef7]"
+            }
+          >
             Authority Connect
           </Link>
         </li>
-        <li>
-          <Link href="/team" className={currentRoute === '/team' ? 'text-[#d69ef7] font-bold' : 'hover:text-[#d69ef7]'}>
+        <li className="hover:scale-105">
+          <Link
+            href="/team"
+            className={
+              currentRoute === "/team"
+                ? "text-[#d69ef7] font-bold"
+                : "hover:text-[#d69ef7]"
+            }
+          >
             Team
           </Link>
         </li>
-        <li>
-          <Link href="/contact-us" className={currentRoute === '/contact-us' ? 'text-[#d69ef7] font-bold' : 'hover:text-[#d69ef7]'}>
+        <li className="hover:scale-105">
+          <Link
+            href="/contact-us"
+            className={
+              currentRoute === "/contact-us"
+                ? "text-[#d69ef7] font-bold"
+                : "hover:text-[#d69ef7]"
+            }
+          >
             Contact us
           </Link>
         </li>
-        <li>
+        <li className="hover:scale-105">
           {/* Profile Handling */}
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
           </SignedIn>
           <SignedOut>
-            <Link href="/profile" className={currentRoute === '/profile' ? 'text-blue-600 font-bold' : 'hover:text-blue-600'}>
+            <Link
+              href="/profile"
+              className={
+                currentRoute === "/profile"
+                  ? "text-[#d69ef7] font-bold"
+                  : "hover:text-[#d69ef7]"
+              }
+            >
               Profile
             </Link>
           </SignedOut>
